a48b42c8d919181539f8d8e7760917f78b1de06d
leon <leonfrer@gmail.com>
2020-08-31 18:33:57 +0800

    socket server

---
+++ src/main/java/com/github/socket/SocketServer.java
@@ -0,0 +1,26 @@
+package com.github.socket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class SocketServer {
+
+	public static void main(String[] args) throws InterruptedException {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup employeeGroup = new NioEventLoopGroup();
+
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(bossGroup, employeeGroup).channel(NioServerSocketChannel.class).childHandler(null);
+
+			ChannelFuture channelFuture = serverBootstrap.bind(8889).sync();
+			channelFuture.channel().closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			employeeGroup.shutdownGracefully();
+		}
+	}
+}
